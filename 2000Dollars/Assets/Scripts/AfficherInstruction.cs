82d7df059c0f71b98327cbdcb2c812354b53fa5b
Valentin Dequidt <31954676+PaillassonAsthmatique@users.noreply.github.com>
2018-03-10 11:19:56 -0500

    #3
    naviguation Menu Princiapl/Instruction

---
+++ 2000Dollars/Assets/Scripts/AfficherInstruction.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AfficherInstruction : MonoBehaviour
+{
+
+    public GameObject instruction2;
+    public GameObject menuPrincipal2;
+
+    public void allerInstruction()
+    {
+
+        GestionLevel.recommencer();
+
+        instruction2.SetActive(true);
+        menuPrincipal2.SetActive(false);
+    }
+
+}
---
+++ 2000Dollars/Assets/Scripts/AllerMenuPrincipal.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AllerMenuPrincipal : MonoBehaviour {
+
+    public GameObject instruction;
+    public GameObject menuPrincipal;
+
+    public void allerMenuPrincpal()
+    {
+
+        GestionLevel.recommencer();
+
+        instruction.SetActive(false);
+        menuPrincipal.SetActive(true);
+    }
+
+}
