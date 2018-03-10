39609e29e10acd90baf7315f7eff1903fcf79827
Flo <flolevy33@gmail.com>
2018-03-09 22:34:57 -0500

    Creation script gestion clique + fonctions

---
+++ 2000Dollars/Assets/Scripts/SuiviChien.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SuiviChien : MonoBehaviour {
+
+
+    public GameObject cible;
+    public static float vitesse = 1.25f;
+    private Rigidbody body;
+    private Vector3 targetPosition;
+    private bool enMouvement;
+
+
+	// Use this for initialization
+	void Start () {
+
+        targetPosition = transform.position;
+        enMouvement = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(Input.GetMouseButton(0))
+        {
+            setPositionAveugle();    
+        }
+        if(Input.GetMouseButton(1))
+        {
+            stopAveugle();
+        }
+	
+
+	}
+
+    void setPositionAveugle()
+    {
+    }
+
+    void stopAveugle()
+    {
+
+    }
+}
