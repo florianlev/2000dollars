0fcc314dee9d0acfe872b76c8ff61d0c25c9f0b6
Valentin Dequidt <31954676+PaillassonAsthmatique@users.noreply.github.com>
2018-03-10 13:33:04 -0500

    #5
    ajout modification sensibilité souris

--- 2000Dollars/Assets/Scripts/CameraMenu.cs
+++ 2000Dollars/Assets/Scripts/CameraMenu.cs
@@ -8,17 +8,21 @@ public class CameraMenu : MonoBehaviour {
 
     public float vitesse;
 
+    public float vitesseRotation;
+
     public bool showCursor;
+
+    private bool debut = true; 
 
     private void Awake()
     {
-        print(Cursor.visible);
         if ( showCursor == false)
         {
             Cursor.visible = true;
         }
         camera = GetComponent<Camera>();
         vitesse = 2f;
+        vitesseRotation = 3f;   
 
     }
 
@@ -32,6 +36,23 @@ public class CameraMenu : MonoBehaviour {
 
         transform.position -= Vector3.left * vitesse * Time.deltaTime;
 
+        if (transform.position.x > 40)
+        {
+            debut = false;
+            vitesse = -vitesse;
 
-	}
+        }
+        if (transform.position.x > 20)
+        {
+            transform.Rotate(0, Time.deltaTime * vitesseRotation, 0);
+        }
+
+        if (transform.position.x < -4) vitesse = -vitesse;
+
+        if (transform.position.x < 16 && !debut)
+        {
+            transform.Rotate(0, Time.deltaTime * -vitesseRotation, 0);
+
+        }
+    }
 }
