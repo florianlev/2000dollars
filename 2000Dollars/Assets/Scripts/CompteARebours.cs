3b210d81c12da1473a74ff191c67b022ac029b08
Valentin Dequidt <31954676+PaillassonAsthmatique@users.noreply.github.com>
2018-03-09 20:52:14 -0500

    #3
    ajout script compte a rebours

--- 2000Dollars/Assets/Scripts/CompteARebours.cs
+++ 2000Dollars/Assets/Scripts/CompteARebours.cs
@@ -1,16 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class CompteARebours : MonoBehaviour {
+public class CompteARebours : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public float compteARebours;
+
+    public Text compteAReboursAffichage;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+        compteARebours = 120f;
+
+        compteAReboursAffichage.text = "Temps restant : " + compteARebours;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        compteARebours = compteARebours - Time.timeSinceLevelLoad;
+
+        compteAReboursAffichage.text = "Temps restant : " + compteARebours;
+
+
+    }
+
+
+    void finCompteARebours()
+    {
+
+        compteAReboursAffichage = "Game Over !";
+
+    }
+
 }
+
