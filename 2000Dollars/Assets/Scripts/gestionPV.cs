efa8cbd09480b3d365dbdcd0627ff174a3b6f871
Valentin Dequidt <31954676+PaillassonAsthmatique@users.noreply.github.com>
2018-03-10 19:50:06 -0500

    #3
    modification dynamique nombre de cannes restantes

---
+++ 2000Dollars/Assets/Scripts/gestionPV.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class gestionPV : MonoBehaviour {
+
+    public Text labelpv;
+
+
+	void Start () {
+
+        if (GestionLevel.vieCourante == 1) labelpv.text = GestionLevel.vieCourante+" canne restante";
+        else
+        {
+            labelpv.text = GestionLevel.vieCourante + " cannes restantes";
+        }
+
+		
+	}
+	
+	void Update ()
+    {
+
+        if (GestionLevel.vieCourante == 1) labelpv.text = GestionLevel.vieCourante + " canne restante";
+        else
+        {
+            labelpv.text = GestionLevel.vieCourante + " cannes restantes";
+        }
+
+    }
+}
